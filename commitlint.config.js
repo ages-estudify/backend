bb99d1e536b10e1815bcf61c6b29bb0d077397a5
Gabriel Saldanha <gabrielsdnegre@gmail.com>
2026-03-17 12:25:57 -0300

    chore(0000): update commit msg script

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-empty': [2, 'never'], // Torna o que está nos parênteses obrigatório
-    'type-enum': [2, 'always', ['feature', 'fix', 'hotfix', 'chore', 'refactor']],
-    'subject-case': [0], // Permite qualquer case na mensagem para não ser chato
-  },
-};
