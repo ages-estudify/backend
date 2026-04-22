d24b5cb722c7b7c7dfed57b99cfd655d5a267f79
Henrique Garcia <henriquegarcia1597@gmail.com>
2026-04-22 18:17:00 -0300

    feature(86ag3w6n7): add service

---
+++ src/exam/exam.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ExamRepository } from './exam.repository';
+import { ExamListingDto } from './dto/examListing.dto';
+
+@Injectable()
+export class ExamService {
+  constructor(private examRepository: ExamRepository) {}
+
+  async findAllWithLastAttemptByUser(userId: string): Promise<ExamListingDto>{
+    
+    const result = await this.examRepository.findAllWithLastAttemptByUser(userId);
+
+    return {
+    success: true,
+    data: result.map((item) => ({
+      id: item.id,
+      name: item.name,
+      totalQuestions: item.totalQuestions,
+      origin: item.origin,
+      status: item.status,
+      answeredQuestions: item.answeredQuestions,
+      image_url: item.image_url,
+      languages: item.language,
+
+      days: [
+        {
+          day: 1,
+          totalQuestions: item.totalQuestions1,
+          isCompleted: item.isCompleted1,
+         
+        },
+        {
+          day: 2,
+          totalQuestions: item.totalQuestions2,
+          isCompleted: item.isCompleted2,
+        },
+      ],
+    })),
+  };
+}
+}
