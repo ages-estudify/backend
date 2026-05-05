08e2773ae284b029873ccbd9b088c45d11b1511d
ravel-andrade <ravel.gou@gmail.com>
2026-05-04 21:05:14 -0300

    fix

--- prisma/migrations/20260425135008_add_exam_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "ExamStatus" AS ENUM ('DRAFT', 'PUBLISHED');
-
--- AlterTable
-ALTER TABLE "Exam" ADD COLUMN     "status" "ExamStatus" NOT NULL DEFAULT 'DRAFT';
--- prisma/migrations/20260429110829_add_new_exam_status/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterEnum
-ALTER TYPE "ExamStatus" ADD VALUE 'ARCHIVED';
